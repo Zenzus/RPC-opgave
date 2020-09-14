32b8a9b193bea629e11830f5e70b47da6a3555b5
Jonas Hein <jonas.hein25@gmail.com>
2020-09-14 18:13:04 +0200

    Update BankImplementation.java

--- code/P5-RMI-DB-Server/src/main/java/dk/dd/rmi/dbserver/BankImplementation.java
+++ code/P5-RMI-DB-Server/src/main/java/dk/dd/rmi/dbserver/BankImplementation.java
@@ -1,9 +1,9 @@
 package dk.dd.rmi.dbserver;
 
 /**
- *
  * @author Dora Di
  */
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,65 +19,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class BankImplementation extends UnicastRemoteObject implements BankInterface
-{
+public class BankImplementation extends UnicastRemoteObject implements BankInterface {
     // public static String url = "jdbc:h2:mem:Bank";
     public static String url = "jdbc:h2:file:C:/Users/jonas/IdeaProjects/soft2020fall-si/code/P5-RMI-DB-Server/src/main/resources/db/bank";
     public static String user = "sa";
     public static String password = "";
     public static String driver = "org.h2.Driver";
 
-    BankImplementation()throws RemoteException{}
+    BankImplementation() throws RemoteException {
+    }
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name)
-    {
+    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
 
     @GetMapping("/bank")
-    public List<Customer> getMillionaires()
-    {
+    public List<Customer> getMillionaires() {
 
-        List<Customer> list=new ArrayList<Customer>();
-        try
-        {
+        List<Customer> list = new ArrayList<Customer>();
+        try {
             Class.forName(driver);
-            Connection con=DriverManager.getConnection(url, user, password);
-            PreparedStatement ps=con.prepareStatement("select * from Customer where amount >= 100000;");
-            ResultSet rs=ps.executeQuery();
-            while(rs.next())
-            {  
-                Customer c=new Customer();  
-                c.setAccnum(rs.getLong(1));
-                c.setName(rs.getString(2));  
-                c.setAmount(rs.getDouble(3));
-                System.out.println(c);
-                list.add(c);  
-            }  
-            con.close();  
-        }
-        catch(Exception e)
-        {
-            System.out.println(e);
-        }  
-        return list;  
-    }
-
-    @Override
-    public List<Customer> getByName(String cName) throws RemoteException
-    {
-
-        List<Customer> list=new ArrayList<Customer>();
-        try
-        {
-            Class.forName(driver);
-            Connection con=DriverManager.getConnection(url, user, password);
-            PreparedStatement ps=con.prepareStatement("select * from Customer where name= '"+cName+ "' ;");
-            ResultSet rs=ps.executeQuery();
-            while(rs.next())
-            {
-                Customer c=new Customer();
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = con.prepareStatement("select * from Customer where amount >= 100000;");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Customer c = new Customer();
                 c.setAccnum(rs.getLong(1));
                 c.setName(rs.getString(2));
                 c.setAmount(rs.getDouble(3));
@@ -85,9 +52,31 @@ public class BankImplementation extends UnicastRemoteObject implements BankInter
                 list.add(c);
             }
             con.close();
+        } catch (Exception e) {
+            System.out.println(e);
         }
-        catch(Exception e)
-        {
+        return list;
+    }
+
+    @Override
+    public List<Customer> getByName(String cName) throws RemoteException {
+
+        List<Customer> list = new ArrayList<Customer>();
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, user, password);
+            PreparedStatement ps = con.prepareStatement("select * from Customer where name= '" + cName + "' ;");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Customer c = new Customer();
+                c.setAccnum(rs.getLong(1));
+                c.setName(rs.getString(2));
+                c.setAmount(rs.getDouble(3));
+                System.out.println(c);
+                list.add(c);
+            }
+            con.close();
+        } catch (Exception e) {
             System.out.println(e);
         }
         return list;
